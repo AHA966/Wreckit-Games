c8d147e7db38991a11344a08aa2589ab5e226117
psycg5 <psycg5@exmail.nottingham.ac.uk>
2022-12-14 14:21:42 +0000

    Created Main Scene With 8x8 Grid Of 2D Squares
    WREC-6
    
    Scene has grid of squares which change colour when clicked and can check if they are occupied
    but currently it is stuck at 90 degrees rather than flat

---
+++ Assets/GridCell.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCell : MonoBehaviour
+{
+
+    private int Posx;
+    private int Posy;
+
+//refernce to the object on the grid square
+    public GameObject objectInThisGridSpace = null;
+
+//bool if the grid square is occupied or not
+    public bool isOccupied = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetPosition(int x, int y){
+
+        Posx = x;
+        Posy = y;
+    }
+
+    public Vector2Int GetPosition(){
+        return new Vector2Int(Posx,Posy); 
+    }
+}
