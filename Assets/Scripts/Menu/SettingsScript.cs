a46740e2421ecf2c201a643492741c6e262c5b60
unknown <psyaa16@nottingham.ac.uk>
2023-01-10 15:47:56 +0000

    Quality Dropdown Linked and Implemented

--- Assets/Scripts/Menu/SettingsScript.cs
+++ Assets/Scripts/Menu/SettingsScript.cs
@@ -6,31 +6,19 @@ using UnityEngine.UI;
 
 public class SettingsScript : MonoBehaviour
 {
-    /**
+    
     public const int DEFAULT_QUALITY_LEVEL = 2;
     public const string QUALITY_SETTINGS_KEY = "quality"; 
 
-    public Slider VolumeSlider;
-    public Dropdown QualityDropdown;
-    public AudioSource ClickSource;
+    public TMPro.TMP_Dropdown QualityDropdown;
 
     public void Start()
     {
-        // Set slider to correct saved position
-        VolumeSlider.SetValueWithoutNotify(AudioManager.GetMasterVolume());
         
         // Load quality from saved value
         QualityDropdown.value = PlayerPrefs.GetInt(QUALITY_SETTINGS_KEY, DEFAULT_QUALITY_LEVEL);
         // Note: the line above does not call "SetQuality" if its value is unchanged so we call it again below
         SetQuality(QualityDropdown.value);
-    }
-
-    public void SetVolume(float Volume)
-    {
-      AudioManager.SetMasterVolume((int)Volume);
-
-      if(!ClickSource.isPlaying)
-            ClickSource.Play();
     }
 
     public void SetQuality(int qualityIndex)
@@ -43,5 +31,5 @@ public class SettingsScript : MonoBehaviour
     {
       Screen.fullScreen = isFullScreen;
     }
-    **/
+    
 }
